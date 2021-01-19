953aa87799df885e99b4d3c5782fbe3dc8398b96
Manish <manysh23@gmail.com>
2021-01-19 21:40:58 +0545

    Updated .gitignore file and models

--- app.js
+++ app.js
@@ -18,5 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//using router
 app.use('/', indexRouter);
+
+
 module.exports = app;
--- models/seller.js
+++ models/seller.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Seller', {
-    fName: { type: String },
-    lName: { type: String },
-    email: { type: String },
-    mobileNumber: { type: String },
-    password: { type: String },
+    fName: {type: String},
+    lName: {type: String},
+    email: {type: String},
+    mobileNumber: {type: String},
+    password: {type: String},
 });
 module.exports = Product;
