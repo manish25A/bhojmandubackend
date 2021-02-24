fb2c2cddce7081f7c933f385d357ff946b2e78cf
Manish BC <manysh23@gmail.com>
2021-02-24 10:32:03 +0545

    added cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors=require('cors');
 // importing db
 var db = require('./Database/database')
 
@@ -14,6 +14,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
