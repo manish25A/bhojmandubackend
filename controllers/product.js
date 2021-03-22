e160628d136d34febcaccdd409ccaeac9357de1b
Manish <manysh23@gmail.com>
2021-03-22 21:41:39 +0545

    added display all in controllers and routes

--- controllers/product.js
+++ controllers/product.js
@@ -1,18 +1,19 @@
-const ErrorResponse = require("../utils/errorResponse");
-const Product = require("../models/product");
-const asyncHandler = require("../middleware/async");
+const ErrorResponse = require('../utils/errorResponse');
+const Product = require('../models/product');
+const asyncHandler = require('../middleware/async');
 //To get the file name extension line .jpg,.png
-const path = require("path");
-
+const path = require('path');
 
 //--------------------CREATE product------------------
 
 exports.createProduct = asyncHandler(async (req, res, next) => {
-
-  const product = await Product.create({...req.body,vendor:req.user._id});
+  const product = await Product.create({
+    ...req.body,
+    customer: req.user._id,
+  });
 
   if (!product) {
-    return next(new ErrorResponse("Error adding product"), 404);
+    return next(new ErrorResponse('Error adding product'), 404);
   }
 
   res.status(201).json({
@@ -21,23 +22,25 @@ exports.createProduct = asyncHandler(async (req, res, next) => {
   });
 });
 
-// //-------------------Display all products
-//
-//
-// exports.getProducts = asyncHandler(async (req, res, next) => {
-//   const products = await Product.find({}).select('vendor');
-//
-//   res.status(201).json({
-//     success: true,
-//     count: products.length,
-//     data: products,
-//   });
-// });
+//-------------------Display all products
+
+exports.getProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find({}).select('vendor');
+
+  res.status(201).json({
+    success: true,
+    count: products.length,
+    data: products,
+  });
+});
 
 //-------------------Display all products of a vendor
 
 exports.getVendorProducts = asyncHandler(async (req, res, next) => {
-  const products = await Product.find({}).populate({path:'vendor',match:{fname:req.params.fname}});
+  const products = await Product.find({}).populate({
+    path: 'vendor',
+    match: { fname: req.params.fname },
+  });
 
   res.status(201).json({
     success: true,
@@ -49,10 +52,13 @@ exports.getVendorProducts = asyncHandler(async (req, res, next) => {
 // -----------------FIND Product BY ID-------------------
 
 exports.getProductById = asyncHandler(async (req, res, next) => {
-  const product = await Product.findById({_id:req.params.id, vendor:req.user._id}).populate('customer');
+  const product = await Product.findById({
+    _id: req.params.id,
+    vendor: req.user._id,
+  }).populate('customer');
 
   if (!product) {
-    return next(new ErrorResponse("Student not found"), 404);
+    return next(new ErrorResponse('Student not found'), 404);
   }
 
   res.status(200).json({
@@ -86,9 +92,11 @@ exports.productImageUpload = asyncHandler(async (req, res, next) => {
 
   console.log(product);
   if (!product) {
-    return next(new ErrorResponse(`No student found with ${req.params.id}`), 404);
+    return next(
+      new ErrorResponse(`No student found with ${req.params.id}`),
+      404
+    );
   }
-
 
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
@@ -104,10 +112,10 @@ exports.productImageUpload = asyncHandler(async (req, res, next) => {
   // Check file size
   if (file.size > process.env.MAX_FILE_UPLOAD) {
     return next(
-        new ErrorResponse(
-            `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
-            400
-        )
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
     );
   }
 
